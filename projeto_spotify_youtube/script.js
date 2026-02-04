2c9dba37e0c6bb83c9459f5dda5e19f7108faf6d
Lisboa_dev <113394648+Arthur-Alves-Lisboa@users.noreply.github.com>
2026-02-04 05:05:35 -0300

    Add files via upload

---
+++ projeto_spotify_youtube/script.js
@@ -0,0 +1,81 @@
+let musicas = [
+    {titulo: 'Rock 1', artista: 'Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig', src: 'musicas/Celestial Distortion - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3', img: 'imagens/rock.jpg'},
+    {titulo: 'Rock Pesado', artista: 'Linkin Park', src: 'musicas/Demented 90s Cartoon Theme - Rod Kim.mp3', img: 'imagens/rock2.jpg'},
+    {titulo: 'Rock Metal', artista: 'Kerian', src: 'musicas/Swimming In A Fish Bowl - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3', img: 'imagens/rock3.jpg'},
+];
+
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
+
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if(indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => { 
+    indexMusica++;
+    if(indexMusica > 2) {   
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+//Funções
+function renderizarMusica(index) {
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica() {
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+
+function pausarMusica() {
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress');
+   barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100)  + '%';
+   let tempoDecorrido = document.querySelector('.inicio');
+   tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos) {
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if(campoSegundos < 10) {
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos + ':' + campoSegundos;
+}
+
